b9ddc65e87905b2fa0122c7d035def986921d9ee
Miryam Bathilde <mbathilde@gmail.com>
2022-05-07 13:20:01 +0200

    added react router hotels for list component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
 } from "react-router-dom";
 import Home from "./pages/home/Home";
+import List from "./pages/list/List";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
       </Routes>
     </BrowserRouter>
   );
