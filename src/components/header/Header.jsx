d2220a9a93b1f4a57317bd5466a2c9e51c1847c0
Miryam Bathilde <mbathilde@gmail.com>
2022-05-07 16:23:41 +0200

    added fontawesome and header to project

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./header.scss"
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerContainer">
+            <div className="headerList">
+                <div className="headerListItem active">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Stays</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Flights</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Car rentals</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Attractions</span>
+                </div>
+                <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Airport taxis</span>
+                </div>
+            </div>
+            <h1 className="headerTitle">
+              A lifetime of discounts? It's Genius.
+            </h1>
+            <p className="headerDesc">
+              Get rewarded for your travels – unlock instant savings of 10% or
+              more with a free Booking account
+            </p>
+            <button className="headerBtn">Sign in / Register</button>
+        </div>
+    </div>
+
+  )
+}
+
+export default Header
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import "./home.scss"
 
 const Home = () => {
 	return (
-		<div><Navbar/></div>
+		<div>		
+			<Navbar/>
+			<Header/>
+		</div>
 	);
 };
 
