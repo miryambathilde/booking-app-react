f7779b3974825343478278a2e0154c8bbc3f8be7
Miryam Bathilde <mbathilde@gmail.com>
2022-05-10 09:36:06 +0200

    added footer component at home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MailList from '../../components/emailList/mailList';
 import Featured from '../../components/featured/Featured';
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
+import Footer from '../../components/footer/Footer';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import PropertyList from '../../components/propertyList/PropertyList';
@@ -19,6 +20,7 @@ const Home = () => {
 				<h1 className="homeTitle">Homes guests love</h1>
 				<FeaturedProperties />
 				<MailList />
+				<Footer />
 			</div>
 		</div>
 	);
