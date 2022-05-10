7747f82eb00f3e8612a517e82cf51e5f998fa385
Miryam Bathilde <mbathilde@gmail.com>
2022-05-10 09:18:35 +0200

    added featured properties to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Featured from '../../components/featured/Featured';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import PropertyList from '../../components/propertyList/PropertyList';
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
 	return (
-		<div>		
-			<Navbar/>
-			<Header/>
+		<div>
+			<Navbar />
+			<Header />
 			<div className="homeContainer">
-				<Featured/>
+				<Featured />
 				<h1 className="homeTitle">Browse by property type</h1>
-				<PropertyList/>
+				<PropertyList />
+				<h1 className="homeTitle">Homes guests love</h1>
+				<FeaturedProperties />
 			</div>
 		</div>
 	);
